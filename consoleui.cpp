f3163f056418a9eb58a31033f19c96e12225c98e
LogiGudmann <logi@gudmann.is>
2014-11-28 15:34:02 +0000

    modified:   consoleui.cpp

--- consoleui.cpp
+++ consoleui.cpp
@@ -14,21 +14,22 @@ void ConsoleUI::Search()
 {}
 
 void ConsoleUI::start() {
-    int input;
 
     while(1){
     cout << "Welcome!\nChoose one of the following:\n1. List\n2. Search\n3. Insert person\n4. Quit" << endl;
+    char input;
 
     cin >> input;
 
+
     switch(input){
-    case 1: List();
+    case '1': List();
         break;
-    case 2: Search();
+    case '2': Search();
         break;
-    case 3: AddPerson();
+    case '3': AddPerson();
         break;
-    case 4: return;
+    case '4': return;
         break;
     default:
         cout << "Invalid input." << endl;
