bf5b4544f5dbb47aecfe29bef8c159a43344aaac
lommi <jonorn@gmail.com>
2014-11-28 01:18:16 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
 #include "main.h"
 #include "sk1data.h"
 
-
-
-<<<<<<< HEAD
-int main()
-=======
-
 int main(int argc, char *argv[])
->>>>>>> 0273a9cff1f63ca785b72b078bd5f34788b55086
 {
     sk1Data gogn;
     gogn.add_person(QString("hehe"), 1, 2, 3);
