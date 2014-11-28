610e93a3192ba760eb419a2cb6c624cba9f7fd66
lodmfjord <jonorn@gmail.com>
2014-11-28 14:11:42 +0000

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -2,59 +2,8 @@
 #include "sk1data.h"
 
 
-void debug_print(QVector<person> buffer)
-{
-
-    QTextStream(stdout) << "" << endl;
-
-    foreach(person item, buffer)
-    {
-
-        QTextStream(stdout) << "id:" << item.id << "\t"
-                            << "name:" << item.name << "\t"
-                            << "gender:" << item.gender << "\t"
-                            << "year born:" << item.year_born << "\t"
-                            << "year died:" << item.year_death << endl;
-
-
-    }
-
-    QTextStream(stdout)  << "______" << "" << endl;
-}
-
-
 int main(int argc, char *argv[])
 {
-    sk1Data gogn;
-    gogn.add_person(QString("Pall Ivan"), 0, 1981, 2020);
-    gogn.add_person(QString("a"), 1, 10, 3);
-    gogn.add_person(QString("hahahehe"), 1, 444, 3);
 
-    QTextStream(stdout) << "All without sorting" << endl;
-    QVector<person> data = gogn.query();
-    debug_print(data);
-
-    QTextStream(stdout) << "haha without sorting" << endl;
-    data = gogn.query(GET_BY_NAME, "haha");
-    debug_print(data);
-
-
-    QTextStream(stdout) << "gender 0 without sorting" << endl;
-    data = gogn.query(GET_BY_GENDER, "0");
-    debug_print(data);
-
-    QTextStream(stdout) << "all with sorting" << endl;
-    data = gogn.query(GET_ALL, "", SORT_BY_NAME);
-    debug_print(data);
-
-
-    QTextStream(stdout) << "all with sorting descending" << endl;
-    data = gogn.query(GET_ALL, "", SORT_BY_NAME, 100);
-    debug_print(data);
-
-
-    QTextStream(stdout) << "all with sorting year born" << endl;
-    data = gogn.query(GET_ALL, "", SORT_BY_YEARBORN);
-    debug_print(data);
 }
 
