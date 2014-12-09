6de221bb49d458224e9794bd677522bcc87b6b91
lodmfjord <jonorn@gmail.com>
2014-12-09 18:48:11 +0000

    modified:   sqlite.cpp 	modified:   sqlite.h

--- sqlite.cpp
+++ sqlite.cpp
@@ -2,29 +2,191 @@
 #include <iostream>
 sqlite::sqlite()
 {
-    db = QSqlDatabase::addDatabase("QSQLITE");/* Skilgreinir hvaða SQL dræver hún á að nota */
+    /* Skilgreina default */
+    DefaultSort["sortby"] = QString("");
+    DefaultSort["sortorder"] = ASC;
+    TablesDef[QString("persons")] = {"Persons_ID", "Persons_Name",
+                                      "Persons_Sex","Persons_YearBorn",
+                                      "Persons_YearDeath"};
+    TablesDef[QString("computers")] = {"Computers_ID", "Computers_Name",
+                                        "Computers_YearBuilt", "Computers_Type",
+                                        "Computers_Type", "Computers_BultOrNot"};
+    TablesDef[QString("owners")] = {"Persons_ID", "Computers_ID"};
+
+    /* Skilgreinir hvaða SQL dræver hún á að nota */
+    db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("Skil2.sqlite");
-    searchQuery = QSqlQuery(db);
-    tables = table.split(", ");
+
     if (db.isValid())
     {
 
         db.open();
         if (db.isOpen())
         {
+            qDebug ("Database is open!");
+            return;
 
         }
     }
-    else
-    {
-        /* villu skilaboð */
-    }
+
+
 }
-void sqlite::query(QString WHAT=QString("*"),
-                   QString TABLE=QString("Persons"),
-                   QString SORT=QString(""))
+QString sqlite::searchstring(QHash<QString,QString> WHAT)
 {
-    searchQuery.prepare("SELECT "+WHAT+" FROM "+TABLE + " "+SORT);
-    searchQuery.exec();
-    // while(searchQuery.next()){}
+    QString searchstring = "";
+    int a = 0;
+    foreach (QString string, WHAT.keys())
+    {
+        if (a != 0) searchstring = searchstring + " AND ";
+        else
+        {
+            searchstring = " WHERE ";
+            searchstring = searchstring + string + "=" + WHAT[string];
+        }
+    }
+
+    return searchstring;
 }
+
+QString sqlite::sortstring(QHash<QString,QString> SORT)
+{
+    QString sort = "";
+    if (SORT["sortorder"] != ASC && SORT["sortorder"] != DESC) SORT["sortorder"] = ASC;
+    if (SORT["sortby"] == "") sort = "";
+    else sort = " ORDER BY "+ SORT["sortby"] + " " + SORT["sortorder"];
+
+    return sort;
+}
+
+QHash<int, QHash<QString, QString> > sqlite::query(QString TABLE,
+                   QHash<QString,QString> WHAT,
+                   QHash<QString,QString> SORT)
+{
+    QHash<int, QHash<QString, QString> > buffer;
+    // Tjekka hvort sé valid tafla
+    if (!tables.contains(TABLE))
+    {
+        qDebug() << "Table " << TABLE << " is not valid!";
+        return buffer;
+    }
+
+    QString getstring = "";
+    int a = 0;
+    foreach (QString string, TablesDef[TABLE])
+    {
+        if (a == 0) getstring = string;
+        else getstring = getstring + "," + string;
+        a++;
+    }
+
+    QSqlQuery searchQuery;
+    searchQuery = QSqlQuery(db);
+    QString searchWHERE = searchstring(WHAT);
+    QString sortSTRING = sortstring(SORT);
+    QString SQL = "SELECT "+getstring+" FROM "+TABLE+searchWHERE+sortSTRING;
+    searchQuery.clear();
+    searchQuery.prepare(SQL);
+    if (!searchQuery.exec())
+    {
+        qDebug() << "SQL QUERY ERROR:" << searchQuery.lastError().text();
+        return buffer;
+    }
+    int c = 0;
+    while(searchQuery.next())
+    {
+        QHash<QString, QString> buffer2;
+        int d = 0;
+        foreach (QString string, TablesDef[TABLE])
+        {
+            buffer2.insert(string, searchQuery.value(d).toString());
+            d++;
+        }
+        buffer.insert(c, buffer2);
+        c++;
+    }
+    return buffer;
+}
+
+/* INSERT */
+void sqlite::insert(QString TABLE, QHash<QString, QString> insert)
+{
+    if (!tables.contains(TABLE))
+    {
+        qDebug() << "Table " << TABLE << " is not valid!";
+        return;
+    }
+
+    QString rows;
+    int a = 0;
+    foreach (QString string, insert.keys())
+    {
+       if (!TablesDef[TABLE].contains(string))
+       {
+           qDebug() << "Invalid column " << string << " in table " << TABLE;
+           return;
+       }
+
+
+       if (a == 0) rows = " ("+string;
+       else rows = rows + ", " + string;
+        a++;
+    }
+    rows = rows + ") VALUES ";
+
+    a= 0;
+    foreach (QString string, insert.keys())
+    {
+        if (a == 0) rows = rows + "(" + QString(":")+string;
+        else rows = rows + ", " + ":"+string;
+        a++;
+    }
+
+    rows = rows + ")";
+
+    QString sql = "INSERT INTO "+TABLE+rows;
+
+    QSqlQuery searchQuery;
+    searchQuery = QSqlQuery(db);
+    searchQuery.clear();
+
+    searchQuery.prepare(sql);
+    foreach (QString string, insert.keys())
+    {
+
+        searchQuery.bindValue(":"+string, insert[string]);
+    }
+
+    searchQuery.exec();
+
+
+
+
+    qDebug() << sql;
+
+}
+
+void sqlite::test()
+{
+    query("persons", EmptySearch, DefaultSort);
+    QHash<QString, QString> ble;
+    ble.insert("Persons_Name","Heheh");
+    ble.insert("Persons_Sex","0");
+    ble.insert("Persons_YearBorn","1983");
+    ble.insert("Persons_YearDeath","2014");
+
+
+
+    insert("persons", ble);
+
+}
+
+
+const QString sqlite::ASC = QString("ASC");
+const QString sqlite::DESC = QString("DESC");
+
+const int sqlite::PERSONS_TABLE = 0;
+const int sqlite::OWNERS_TABLE = 1;
+const int sqlite::COMPUTERS_TABLE = 2;
+
+const QStringList sqlite::tables = {"persons", "owners", "computers"};
+
--- sqlite.h
+++ sqlite.h
@@ -2,10 +2,6 @@
 #define SQLITE_H
 #include <QtSql>
 
-static const int PERSONS_TABLE 0;
-static const int OWNERS_TABLE 1;
-static const int COMPUTERS_TABLE 2;
-
 
 
 
@@ -13,12 +9,32 @@ class sqlite
 {
 public:
     sqlite();
-    get_
+    static const QString ASC;
+    static const QString DESC;
+    static const int PERSONS_TABLE;
+    static const int OWNERS_TABLE;
+    static const int COMPUTERS_TABLE;
+    QHash<QString, QString> DefaultSort;
+    QHash<QString,QString> EmptySearch;
+    void test();
+    void insert(QString TABLE, QHash<QString, QString> insert);
+
+    QHash<int, QHash<QString, QString> > query(QString TABLE,
+                       QHash<QString,QString> WHAT,
+                       QHash<QString,QString> SORT);
+
 
 private:
-   const QString table = "Persons, Owners, Computers";
-   QStringList tables;
-   /* Taflan Person er
+   static const QStringList tables;
+   static const QStringList PersonsTable;
+   static const QStringList ComputersTable;
+   static const QStringList OwnersTable;
+   QHash<QString, QStringList> TablesDef;
+   QString searchstring(QHash<QString,QString> WHAT);
+   QString sortstring(QHash<QString,QString> SORT);
+
+
+   /* Taflan Persons er
     * Persons_ID(int) Persons_Name(String) Person_Sex(String) Person_YoB(int) Person_YoD(int)
     *
     * Taflan Owners er
@@ -28,8 +44,10 @@ private:
     * Computers_ID(int) Name(string) YearBuilt (int) Type (string) BuiltOrNot(bool)
 */
    QSqlDatabase db;
-   QSqlQuery searchQuery;
-   void query(QString WHAT, QString TABLE, QString SORT);
+
+
+
+
 
 };
 
